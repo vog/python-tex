8e9ac1fcfcb15a15baad0c044ab59450db1857f6
Volker Grabsch <vog@notjusthosting.com>
2009-08-21 04:28:17 +0200

    add setup.py, improved metadata
    darcs-hash:ce9b798469c7c645cf992f1ae1b50a9ca5ca9fe8

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import setup
+import tex as module
+
+setup(
+    name             = module.__name__,
+    version          = module.__version__,
+    description      = module.__doc__.split('\n')[0],
+    long_description = '\n'.join(module.__doc__.split('\n')[1:]).strip('\n'),
+    author           = module.__author__,
+    author_email     = module.__author_email__,
+    url              = module.__url__,
+    py_modules       = [module.__name__],
+    classifiers      = module.__classifiers__.strip('\n').split('\n'),
+)
--- tex.py
+++ tex.py
@@ -1,28 +1,42 @@
-"""Convert LaTeX or TeX source to PDF or DVI, and escape strings for LaTeX.
+'''Convert LaTeX or TeX source to PDF or DVI, and escape strings for LaTeX.'''
 
-:Version:   0.1
-:Requires:  Python 2.4 or higher
-:Author:    Volker Grabsch <vog@notjusthosting.com>
-:License:
-    Permission is hereby granted, free of charge, to any person obtaining
-    a copy of this software and associated documentation files (the
-    "Software"), to deal in the Software without restriction, including
-    without limitation the rights to use, copy, modify, merge, publish,
-    distribute, sublicense, and/or sell copies of the Software, and to
-    permit persons to whom the Software is furnished to do so, subject
-    to the following conditions:
+__version__      = '0.1'
+__author__       = 'Volker Grabsch'
+__author_email__ = 'vog@notjusthosting.com'
+__url__          = 'http://www.profv.de/python-tex/'
+__classifiers__  = '''
+Development Status :: 5 - Production/Stable
+Development Status :: 6 - Mature
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Documentation
+Topic :: Office/Business
+Topic :: Printing
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Text Processing :: Markup :: LaTeX
+'''
+__license__      = '''
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject
+to the following conditions:
 
-    The above copyright notice and this permission notice shall be
-    included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
 
 import string
 import random
