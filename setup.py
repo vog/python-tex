f7212c14f958cd7b63ff2e88836c01d26db0a087
Volker Grabsch <vog@notjusthosting.com>
2010-09-15 15:14:42 +0200

    simplified setup.py
    darcs-hash:84a0e7e03d56cd343f55590733f36af23d46be90

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import re
+import setuptools
+
 import tex as module
 
-setup(
+setuptools.setup(
     name             = module.__name__,
     version          = module.__version__,
-    description      = module.__doc__.split('\n')[0],
-    long_description = '\n'.join(module.__doc__.split('\n')[1:]).strip('\n'),
+    description      = module.__doc__.partition('\n\n')[0],
+    long_description = module.__doc__.partition('\n\n')[2],
     author           = module.__author__,
     author_email     = module.__author_email__,
     url              = module.__url__,
     py_modules       = [module.__name__],
-    classifiers      = [c.strip() for c in module.__classifiers__.strip().split('\n')],
+    classifiers      = re.findall(r'\S[^\n]*', module.__classifiers__),
 )
